9d51b5ea1d806686c7b1b44b55c0da03fc0358f6
zeegomo <g.pasini98@gmail.com>
2019-02-01 21:59:21 +0100

    simple jersey server

---
+++ src/main/java/test/HelloWorldResource.java
@@ -0,0 +1,15 @@
+package test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("test")
+public class HelloWorldResource {
+	
+	@GET
+	@Produces("text/plain")
+	public String getGreetings(){
+		return "ciao";
+	}
+}
